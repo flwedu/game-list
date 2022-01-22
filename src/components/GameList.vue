669ed6b1b6d9cfd15e0cb8a99c9f9b036595ba25
Eduardo Aquino <edutraquino@gmail.com>
2022-01-22 11:35:30 -0300

    create GameList component

---
+++ src/components/GameList.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>{{ game.name }}, released in {{ game.year }}</li>
+</template>
+
+<script>
+export default {
+  name: "game-list",
+  props: {
+    game: {
+      id: Number,
+      name: String,
+      year: Number,
+    },
+  },
+};
+</script>
+
+<style></style>
