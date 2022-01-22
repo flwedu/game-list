b4bc786aa2330ea8dd54fe5533f9089c272866f2
Eduardo Aquino <edutraquino@gmail.com>
2022-01-22 11:35:09 -0300

    create Info component

---
+++ src/components/Info.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>{{ infoData.title }}</h1>
+    <p>{{ infoData.description }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Info",
+  props: {
+    infoData: {
+      title: String,
+      description: String,
+    },
+  },
+};
+</script>
+
+<style></style>
