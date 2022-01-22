221c62c736fcdb37fbe9ce9036593e3e252cfbce
Eduardo Aquino <edutraquino@gmail.com>
2022-01-22 11:35:20 -0300

    create gameForm component

---
+++ src/components/GameForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <form name="newGame" autocomplete="off">
+    <input type="text" name="name" id="name" placeholder="name" />
+    <input type="text" name="year" id="year" placeholder="year" />
+    <button type="button" v-on:click="add">Add</button>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    add: () => {
+      console.log("button clicked");
+    },
+  },
+};
+</script>
+
+<style></style>
